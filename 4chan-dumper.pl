2567477d23de32809fd95221e10e6a8a5bd4065e
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2014-11-07 22:52:24 +0100

    cleanup before a public release

--- 4chan-dumper.pl
+++ 4chan-dumper.pl
@@ -1,4 +1,20 @@
 #!/usr/bin/env perl
+########################################################################
+# Copyright (C) 2012-2014 Wojciech Siewierski                          #
+#                                                                      #
+# This program is free software; you can redistribute it and/or        #
+# modify it under the terms of the GNU General Public License          #
+# as published by the Free Software Foundation; either version 3       #
+# of the License, or (at your option) any later version.               #
+#                                                                      #
+# This program is distributed in the hope that it will be useful,      #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of       #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        #
+# GNU General Public License for more details.                         #
+#                                                                      #
+# You should have received a copy of the GNU General Public License    #
+# along with this program. If not, see <http://www.gnu.org/licenses/>. #
+########################################################################
 
 use warnings;
 use strict;
@@ -52,13 +68,13 @@ for (@ARGV) {
         my $url = "http://images.4chan.org/$board/src/$filename";
         print "$url ... ";
 
-        print "[1;33mEXISTS[0m\n" and next if -e $filename;
+        print "\e[1;33mEXISTS\e[0m\n" and next if -e $filename;
 
         my $image;
         if ($dl->($url, \$image) == 0) {
-            print "[1;32mDONE[0m"
+            print "\e[1;32mDONE\e[0m"
         } else {
-            print "[1;31mERROR[0m"
+            print "\e[1;31mERROR\e[0m"
         }
 
         open(my $file, '>', $filename) or next;
